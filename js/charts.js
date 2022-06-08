e4f7097b0b53a5695af36af735b7b97de6a0225e
Sean Flinn <Sean.Flinn1@gmail.com>
2022-06-08 19:32:43 -0400

    update

--- js/charts.js
+++ js/charts.js
@@ -117,7 +117,7 @@ function buildCharts(sample) {
   var newMetaData = data.metadata;
 
   // Create a variable that holds the first sample in the array.
-  var arraySample = newMetaData.filter(obj => obj.id == sampleNames);
+  var arraySample = newMetaData.filter(obj => obj.id == sample);
 
   // 2. Create a variable that holds the first sample in the metadata array.
   var sample1 = arraySample[0];
@@ -129,15 +129,6 @@ function buildCharts(sample) {
 
   // 3. Create a variable that holds the washing frequency.
   var washFreq = sample1.wfreq; 
-  // Create the yticks for the bar chart.
-  var yticks = id1.map(sampleobj => "otu " + sampleObj).slice(0,10).revers();
-  console.log(yticks); 
-  // Use Plotly to plot the bar data and layout.
-  Plotly.newPlot("bar", barData, barLayout);
-    
-  // Use Plotly to plot the bubble data and layout.
-  Plotly.newPlot("bubble",bubbleData,bubbleLayout) 
-   
     
   // 4. Create the trace for the gauge chart.
   var gaugeData = [{
@@ -166,6 +157,6 @@ function buildCharts(sample) {
   };
 
   // 6. Use Plotly to plot the gauge data and layout.
-  Plotly.newPlot("gauge", gaugeData, gaugeLayout);
-});
+  Plotly.newPlot("gauge", gaugeData, gaugeLayout)
+  });
 }
